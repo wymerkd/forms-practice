d2fd70516226b143b99fac5ff95312b1201f4add
wymerkd <wymerkd@gmail.com>
2019-02-13 10:10:12 -0800

    add select form

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
     var startTimeInput= $("input#startTime").val();
     var endTimeInput = $("input#endTime").val();
     var commentsInput = $("input#comments").val();
+    var beverageValue = $("#beverage option:selected").val();
 
     $(".yourName").text(yourNameInput);
     $(".date").text(dateInput);
     $(".startTime").text(startTimeInput);
     $(".endTime").text(endTimeInput);
     $(".comments").text(commentsInput);
+    $("#beverage").text(beverageValue);
+
 
     $("#confirmation").show();
 
